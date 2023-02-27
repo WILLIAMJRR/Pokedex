81a6baf0baa9d2fb2610381976e7951f705cb157
WILLIAMJRR <122072865+WILLIAMJRR@users.noreply.github.com>
2023-02-27 19:56:09 -0300

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import videopokemon from '../video/Pokemon Ash_Satoshi Campeón Mundial Latino EDIT (1080p_30fps_H264-128kbit_AAC).mp4';
 import homecss from './style/home.css';
+import pokelogo from '../img/International_Pokémon_logo.svg.png'
 //paso 7
 const Home = () => {
     //paso 7.4
@@ -30,7 +31,7 @@ const Home = () => {
             <div className='home_img_con'>
                 <img
                     className='home_img'
-                    src='/src/img/International_Pokémon_logo.svg.png'
+                    src={pokelogo}
                     alt=''
                 />
             </div>
